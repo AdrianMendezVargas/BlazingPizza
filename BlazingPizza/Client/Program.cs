1c5fe057136b0349c225fc08bb2d4078f0d96d04
Adrian Alcides Mendez Vargas <adrian_mendez@ucne.edu.do>
2020-09-11 22:12:11 -0400

    Sesion #5. Usando el patron AppState.
    *Manteniendo el estado de la orden al cambiar entre componentes.*
    
    -Creando la clase OrderState y agregandola al injector de dependencias.

--- BlazingPizza/Client/Program.cs
+++ BlazingPizza/Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BlazingPizza.Client.Services;
 
 namespace BlazingPizza.Client {
     public class Program {
@@ -15,6 +16,7 @@ namespace BlazingPizza.Client {
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<OrderState>(); //Agregando OrderState al contenedor de DI
 
             await builder.Build().RunAsync();
         }
---
+++ BlazingPizza/Client/Services/OrderState.cs
@@ -0,0 +1,45 @@
+﻿using BlazingPizza.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client.Services {
+    public class OrderState {  //AppState
+
+
+        public Pizza ConfiguringPizza { get; private set; }
+        public bool ShowingConfigureDialog { get; private set; }
+        public Order Order { get; private set; } = new Order();
+
+        public void ShowConfigurePizzaDialog(PizzaSpecial special) {
+            ConfiguringPizza = new Pizza() {
+                Special = special ,
+                SpecialId = special.Id ,
+                Size = Pizza.DefaultSize ,
+                Toppings = new List<PizzaTopping>()
+            };
+            ShowingConfigureDialog = true;
+        }
+
+        public void CancelConfigurePizzaDialog() {
+            ConfiguringPizza = null;
+            ShowingConfigureDialog = false;
+        }
+
+        public void ConfirmConfigurePizzaDialog() {
+            Order.Pizzas.Add(ConfiguringPizza);
+            ConfiguringPizza = null;
+            ShowingConfigureDialog = false;
+        }
+
+        public void RemoveConfiguredPizza(Pizza pizza) {
+            Order.Pizzas.Remove(pizza);
+        }
+
+        public void ResetOrder() {
+            Order = new Order();
+        }
+
+    }
+}

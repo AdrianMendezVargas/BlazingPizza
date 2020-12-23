a30e8a5d2ad0f56a7344ba62f10e3a7d0f0e3d7d
Adrian Alcides Mendez Vargas <adrian_mendez@ucne.edu.do>
2020-12-23 19:52:38 -0400

    Agregando auto-focus al aceptar la orden

---
+++ BlazingPizza.ComponentsLibrary/Extensions/JSRuntimeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.ComponentsLibrary.Extensions {
+    public static class JSRuntimeExtensions {
+        public static ValueTask<bool> Confirm(this IJSRuntime jsRuntime, string message) {
+            return jsRuntime.InvokeAsync<bool>("confirm", message);
+        }
+
+        public static async Task EvalVoidAsync(this IJSRuntime jsRuntime, string script) {
+            if (script.Length > 0) {
+                await jsRuntime.InvokeVoidAsync("eval" , script);
+            }
+        }
+
+        public static async Task<T> EvalAsync<T>(this IJSRuntime jsRuntime , string script) {
+            T Result = default;
+            if (!string.IsNullOrWhiteSpace(script)) {
+                script = $"(function(){{{script}}})()";
+                Result = await jsRuntime.InvokeAsync<T>("eval" , script); //El objeto eval ejecuta el codigo JavaScript que le pases
+            }
+            return Result;
+        }
+    }
+}
